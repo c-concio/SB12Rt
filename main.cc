96f5c76a356e184154e997ac244ed7af416e3923
c-concio <chris-768@hotmail.com>
2019-10-31 18:01:22 -0400

    Changed contents of file

--- main.cc
+++ main.cc
@@ -1,79 +1,14 @@
-;*****************************************************************
-;* This stationery serves as the framework for a                 *
-;* user application (single file, absolute assembly application) *
-;* For a more comprehensive program that                         *
-;* demonstrates the more advanced functionality of this          *
-;* processor, please see the demonstration applications          *
-;* located in the examples subdirectory of the                   *
-;* Freescale CodeWarrior for the HC12 Program directory          *
-;*****************************************************************
-
-; export symbols
-            XDEF Entry, _Startup            ; export 'Entry' symbol
-            ABSENTRY Entry        ; for absolute assembly: mark this as application entry point
-   
-; Include derivative-specific definitions 
-		INCLUDE 'derivative.inc' 
-
-ROMStart    EQU  $4000  ; absolute address to place my code/constant data
-
-; variable/data section
-            ORG RAMStart
-
-; Insert here your data definition.
-sevsegdata dc.b  $BF, $86, $DB, $CF, $E6, $ED, $FD, $87, $FF, $EF, $F7, $FC, $B9, $DE, $F9, $F1
-PAdata dc.b $10, $20, $40, $80
-i ds.b 1
-key ds.b 1
-myptp dc.b $0E, $0D, $0B, $07 
+#include <hidef.h>      /* common defines and macros: file found in CodeWarrior folder */
+#include "derivative.h"      /* derivative-specific definitions (e.g.: PORTA, DDDRK) */
 
 
-; code section
-            ORG   ROMStart
+#define column0 0x01
+#define column2 0x02
+#define column3 0x04
+#define column4 0x08 
 
-Entry:
-_Startup:  
-            LDS   #RAMEnd+1       ; initialize the stack pointer
-            
-
-; PLL code for 24MHz bus speed from a 4/8/16 crystal
-;            sei
-;            ldx #0
-;            bclr CLKSEL,x,%10000000 ; clear bit 7, clock derived from oscclk
-;            bset PLLCTL,x, %01000000 ; Turn PLL on, bit 6 =1 PLL on, bit 6=0 PLL off
-;            ldaa #$05 ; 5+1=6 multiplier
-;            staa SYNR,x
-            ; ldaa #$03 ; divisor=3+1=4, 16*2*6 /4 = 48MHz PLL freq, for 16 MHz crystal
-;            ldaa #$01 ; divisor=1+1=2, 8*2*6 /2 = 48MHz PLL freq, for 8 MHz crystal
-            ; ldaa #$00 ; divisor=0+1=1, 4*2*6 /1 = 48MHz PLL freq, for 4 MHz crystal
-;            staa REFDV,x
-;wait_b3:    brclr CRGFLG,x, %00001000, wait_b3 ; Wait until bit 3 = 1
-;            bset CLKSEL,x, %10000000
-
-; Dragon12-Light Board:
-            ; Port A: Connected to Keypad. Configure it as input/output (DDRA).
-            ; Port B: Connected to LEDs. Configure it as output (DDRB). Also set PJ1 to 0, and set DDRJ as output ($FF)
-            ;         Also connected to all four 7-seg anodes (PB0-PB7: A,B, ...,G, Dec. Point). Positive logic for both LEDs and 7-seg
-            ;
-            ; Port P: The last 4 bits (PP3-PP0) are connected to the cathodes of the 7-seg LEDs. Configure those 4 bits as output (DDRP)
-            ;         7-seg LEDs cathodes: Negative logic. A '0' is required to turn on a particular 7-seg display
-            ; Port H: Connected to DIPSwitches (H7-H0) and Pushbuttons (H3-H0). Configure it as input (DDRH)
-            ;         DIPSwitches (H7-H0): Positive logic. Up: 1, Down: 0
-            ;         Pushbuttons (H3-H0): When pressed, they send a 0 and take precedence over the DIPswitches.
-            ;                              When not pressed, only the DIPswitches are paid attention to..
-            ; Port K: Connected to LCD. Configure it as output
-            
-            ; Port K (3..0), Port P (3..0), Port T, Port A,B, Port H, Port E: Can be used as general I/O (see headers)
-                        
-            ; I/O Configuration
-            movb #$F0, DDRA          ; set Port A (7-4) to be outputs, Port A (3-0) to be inputs
-            movb #$FF, DDRB          ; set Port B to be all output
-            movb #$FF, DDRP          ; set Port P (3 to 0) to be all output (the others, I don't care)
-            movb #$00, DDRH          ; set Port H to be all input
-            movb #$FF, DDRK          ; set Port K to be all output
-            
-
-            ; I/O usage: 7-seg and DIP switches
+/*
+; I/O usage: 7-seg and DIP switches
             ;  pt  g   f   e   d   c   b   a
             ; PB7 PB6 PB5 PB4 PB3 PB2 PB1 PB0
             ;  1   0   1   1   1   1   1   1   =  0xBF = '0' in the 7-segment display
@@ -92,92 +27,92 @@ _Startup:
             ;  1   1   0   1   1   1   1   0   =  0xDE = 'D' in the 7-segment display
             ;  1   1   1   1   1   0   0   1   =  0xF9 = 'E' in the 7-segment display
             ;  1   1   1   1   0   0   0   1   =  0xF1 = 'F' in the 7-segment display
-            
-             ldx #sevsegdata; X <- sevsegdata (X grabs the address)            
-             movb #$00, PTP   ; All four 7-seg displays cathodes are set to 0 (i.e., they are turned on)
-            
-            
-;showDIPSW:  ldaa PTH        ; Data from DIP switches appear in A. Note that PTH(3..0) are also the pushbuttons
-;                            ; Note that data on the 7-seg displays is only valid for DIP switches 3 to 0. (from 0 to F)
-;            ldab A, X       ; B <- [A+sevsegdata].
-                            ; If A=$00, then, B = [sevsegdata] = $BF
-                            ; If A=$03, then, B = [$03+sevsegdata] = $CF
-           
 
-            ldy #PAdata                      
-forever:    clr i
-                                     
-; Loop for i = 0 --> 3
-loop:       ldaa i ; A <- [i]
-            cmpa #4 ; [i]-4            
-            beq next; If [i] = 4 then go to 'next'
-                        
-            movb A,Y,PORTA ; [PORTA] <- [[Y] + [i]]. Write on 4 MSBs of PORTA
-            bsr delay1ms
-            
-            ldaa PORTA ; A <- [PORTA]. Load A (we are interested in the 4 LSBs)
-                        
-            bsr getidx
-            cmpb #$0F
-            bne lol
-            clra
-            bra lolp
+*/
+                                           
 
-lol         movb i, key
-            
-            ; key <- key*4:
-            lsl key 
-            lsl key 
-            
-            ; B <- B + 4*[i]    
-            addb key            
-            
-            ldaa B,X
-            
-lolp:       staa PORTB
-            
-            ;bsr delay1ms
+void main(void) { // in Assembly code, this is treated as a SubRoutine
 
-            
-            inc i
-            bra loop;        
-; End of Loop for i = 0 --> 3
-            
-next:       bra forever
 
-         
-; ***********            
-; Subroutines
-; ***********
-getidx      bita #$08; A and 00001000. If A=00001000 -> the result is not 0
-            bne is3
-            bita #$04; A and 00000100. If A=00000100 -> the result is not 0
-            bne is2
-            bita #$02; A and 00000010. If A=00000010 -> the result is not 0
-            bne is1
-            bita #$01; A and 00000001. If A=00000001 -> the result is not 0
-            bne is0
-            ldab #$0f
-            bra endi
-            
-is3:        ldab #3
-            bra endi
-is2:        ldab #2
-            bra endi
-is1:        ldab #1
-            bra endi
-is0:        ldab #0
-endi        rts                                                
+  // Variables
+  bool keyPressed = false;
+  int keypadValue[8];                      
+  
+
+
+  // Data Direction Register Setup
+  DDRA = 0xF0;  // Keypad
+  DDRB = 0xFF;  // Cathodes of 7Seg
+  DDRP = 0xFF;  // 7Seg
+  
+  while(true){
     
-; It generates a delay of 1 ms + (pshx+pulx=5 cycles = 0.21 ms). So it is basically 1 ms
-delay1ms    pshx
-            
-            ldx #2400 ; #400(4 MHz bus speed by default in Dragon12-Light). #2400 (24MHz bus speed)
-loops:      psha      ; 2 cycles
-            pula      ; 3 cycles
-            nop       ; 1 cycle
-            nop       ; 1 cycle
-            dbne X,loops; ;3 cycles
+    
+    // Get input from keypad
+    while(true){
+      // Scan through all the columns and rows
+      
+      // for all four columns, look through the MSB bits of port A and see if any of them is set
+      // if not set, go throught the other columns
+      
+      
+      // look at column 1 and see if any of the bits are pressed
+      // Set column0
+      PORTA = PORTA OR column0;
+      if(PORTA != column0){
+        keypadValue = PTA;
+          
+      }
+      
+      
+      
+      
+      
+      
+      
+    }
+    
+    
+    keypadValue[] 
+    
 
-            pulx
-            rts
+
+    // display the note on the 7 segment display
+
+
+
+
+    // output note onto the DAC
+
+
+
+    // DAC then outputs to speaker
+              
+  
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  avg = 0;
+  for (i = 0; i < 10; i++) { 
+      avg = avg + array[i]; // Since avg is signed, the quantities in array[i] will be sign-extended
+  } 
+  avg = avg/10; // integer portion ($1F)
+  asm ("ldd avg"); // inline Assembly instructions  
+  
+  while (1) {
+    
+    DIPSW = PTH; // same as asm("movb PTH, val")
+    LEDS = myOR(DIPSW,avg);         
+  
+    PORTB = LEDS; // or asm("movb LEDS,PORTB")  
+  }
+  
+  
+  
+}
